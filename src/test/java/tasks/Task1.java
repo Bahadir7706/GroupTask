040005222ab8a4b3e9fcea6168aa3eb448e75773
Bahadir <bkucukuysal@gmail.com>
2021-12-22 19:55:55 +0000

    test case received

---
+++ src/test/java/tasks/Task1.java
@@ -0,0 +1,17 @@
+package tasks;
+
+public class Task1 {
+         /*
+    1. Test Case - Automate User Registration process of e-commerce website.
+
+    Steps to Automate:
+    1. Open this url  http://automationpractice.com/index.php
+    2. Click on sign in link.
+    3. Enter your email address in 'Create and account' section.
+    4. Click on Create an Account button.
+    5. Enter your Personal Information, Address and Contact info.
+    6. Click on Register button.
+    7. Validate that user is created.
+     */
+
+}
