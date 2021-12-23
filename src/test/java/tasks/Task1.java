3f352fad71e52034b0e2b060b449bcd1560a766d
AdemFatih <afb3506@gmail.com>
2021-12-23 15:30:18 +0300

    new

--- src/test/java/tasks/Task1.java
+++ src/test/java/tasks/Task1.java
@@ -1,6 +1,9 @@
 package tasks;
 
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("deneme branch");
+    }
          /*
     1. Test Case - Automate User Registration process of e-commerce website.
 
